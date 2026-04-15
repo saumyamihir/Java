3ff911169369b36c5b98136fa06f15a344896fe5
Saumya Mihir <saumyamihir9@gmail.com>
2026-04-15 10:55:44 +0530

    new commit

--- wildcard.java
+++ wildcard.java
@@ -43,33 +43,81 @@
 
 
 // wild Card -> Unknown Type
-// UnBound -> we dont have any boundation.
-// LowerBound -> super Keyword -> Initialize the parent class Variables.
-// UpperBound -> eextends , Inheritance.
-import java.util.*;
-class Box<t>{
+
+// import java.util.*;
+// class Box<t>{
+//     t a;
+//     void setData(t a){
+//         this.a = a;
+//     }
+//     t getData(){
+//         return a;
+//     }
+// }
+// class wildcard{
+//     public static void main(String[] args) {
+//         Box<Integer>b1 = new Box<>();
+//         b1.setData(45);
+//         Box<String>b2 = new Box<>();
+//         b2.setData("Sami");
+//         System.out.println("Integer value : "+b1.getData());
+//         System.out.println("String value : "+b2.getData());
+        
+//         // Wild Card Unknown Type -> "?"
+//         Box<?> box;
+//         box = b1;
+//         System.out.println(box.getData());
+//         box = b2;
+//         System.out.println(box.getData());
+//     }
+// }
+
+
+// UnBounded -> we dont have any boundation.
+// LowerBounded -> super Keyword -> Initialize the parent class Variables.
+// UpperBounded -> extends , Inheritance.
+
+// Object -> Number -> Integer, Float, Double.
+// Number -> Integer, Float, Double.
+
+//import java.util.*;
+class Demo<t>{
     t a;
-    void setData(t a){
+    void set(t a)
+    {
         this.a = a;
     }
-    t getData(){
+    t get()
+    {
         return a;
     }
 }
 class wildcard{
+    static void reader(Demo<? extends Number>d)
+    {
+        System.out.println("Value of a : "+ d.get());
+    }
+    static void writer(Demo<? super Integer>d)
+    {
+        // in super we can update the value
+        // some times we can read the value
+        System.out.println("Value of a :"+ d.get());
+        d.set(78);
+        System.out.println("Value od a: "+d.get());   
+    }
     public static void main(String[] args) {
-        Box<Integer>b1 = new Box<>();
-        b1.setData(45);
-        Box<String>b2 = new Box<>();
-        b2.setData("Sami");
-        System.out.println("Integer value : "+b1.getData());
-        System.out.println("String value : "+b2.getData());
-        
-        // Wild Card Unknown Type -> "?"
-        Box<?> box;
-        box = b1;
-        System.out.println(box.getData());
-        box = b2;
-        System.out.println(box.getData());
+        Demo<Integer>d = new Demo<>(); //object 
+        d.set(34);
+        Demo<String>d1=new Demo<>();
+        d1.set("Sami");
+    
+        Demo<?>d3 =d; //simple variable
+        System.out.println(d3.get());
+        d3 = d1;
+        System.out.println(d3.get());
+
+        // (?) Wild Operator -> Unknown Type
+        reader(d);
+        writer(d);
     }
 }
